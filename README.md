9fa10fbab12e657f20307a3692f10be4ae7ebbc0
Dmitry <dmiandich@yandex.ru>
2024-05-01 03:07:19 +0300

    Version 0.3. Updated according to Fb and VK layout changes

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Browser add-on for removing annoying "Sign Up" message from facebook, instagram 
 Eng:
 Nowadays it's a good idea to minimize amount of information that can be obtained about you by social networks. It is almost impossible to fully avoid using facebook or instagram, but in many cases, all you need there can be read without logging in. Services struggle with unlogged visitors by displaying annoying pop-up windows with sign-up proposition. Such window appears again and again, making it almost impossible to use without signing in.
 
-“SignUp Propo Remover” automatically removes pop-up window and unblock scrolling on facebook, instagram, and vkontakte pages. Current add-on version does not have any settings, just install and use.
+“SignUp Propo Remover” automatically removes pop-up window and unblock scrolling on facebook and vkontakte pages. Current add-on version does not have any settings, just install and use.
 
 Rus:
 В наши дни имеет смысл минимизировать количество информации, которую могут собирать о вас социальные сети. Полностью отказаться от использования фейсбука или инстаграмма почти невозможно, но во многих случаях для ваших целей вполне хватило бы прочитать страницу-другую не заходя в соцсеть под своим именем. Многие сервисы борются с незарегистрированными посетителями, используя надоедливый диалог с приглашением залогинится. Такое окно появляется снова и снова, в самые неподходящие моменты, делая использование для незарегистрированных посетителей почти невозможным.
 
-“SignUp Propo Remover” автоматически убирает окно с приглашением и разблокирует перемотку на просматриваемой странице фейсбука, инстаграма и вконтакте. Текущая версия не имеет настроек, просто установите и пользуйтесь.
+“SignUp Propo Remover” автоматически убирает окно с приглашением и разблокирует перемотку на просматриваемой странице фейсбука и вконтакте. Текущая версия не имеет настроек, просто установите и пользуйтесь.
--- content.js
+++ content.js
@@ -2,60 +2,80 @@ let sitesconf = [
 {site: "vk.com", modify: [
 	{locatetagby: "class", makrname: "UnauthActionBoxContainer", action: "remove"}, 
 	{locatetagby: "class", makrname: "new_header_design", action: "clearstyle"}, 
+    {locatetagby: "body", makrname: "", action: "setstyle", style: "overflow: scroll;"}, 
 	{locatetagby: "id", makrname: "box_layer_bg", action: "remove"},
 	{locatetagby: "id", makrname: "box_layer_wrap", action: "remove"}]},
 {site: "instagram.com", modify: [
 	{locatetagby: "body", makrname: "", action: "clearstyle"}, 
 	{locatetagby: "class", makrname: "RnEpo  _Yhr4    ", action: "remove"}]},
 {site: "facebook.com", modify: [
-	{locatetagby: "id", makrname: "u_0_1t", action: "remove"}]}	
+	{locatetagby: "custom", makrname: "See more on Facebook", action: "remove"},
+	{locatetagby: "custom", makrname: "Please log in to see this page", action: "remove"}]}
 ];
 
 var config = { attributes: false, childList: true, subtree: true }; // Конфигурация MutationObserver
 var mutationCallback = function(reclst, obs) {
-var curl = document.URL;
-
-sitesconf.forEach(function(elem)
-{
-    if(curl.includes(elem.site) == true)
+    var curl = document.URL;
+    
+    sitesconf.forEach(function(elem)
     {
-        var mods = elem.modify;
-        mods.forEach(function(mod)
+        if(curl.includes(elem.site) == true)
         {
-	    var locby = mod.locatetagby;
-	    var nam = mod.makrname;
-	    var act = mod.action;
-	    if(locby == "class")
-	    {
-	      var e = getSingleElementOfClass(nam);
-	    }
-	    if(locby == "id")
-	    {
-	      var e = getSingleElementOfId(nam);
-	    }
-	    if(locby == "body")
-	    {
-	      var e = document.body;
-	    }
-	    
-	    
-	    if(e != undefined)
-	    {
-	      console.log("E = " + e);	      
-	      
-	      if(act == "remove")
-	      {
-		var p = e.parentNode;
-		p.removeChild(e);
-	      }
-	      if(act == "clearstyle")
-	      {
-		e.removeAttribute("style");	      
-	      }		
-	    }	    
-        })
-    }
-})
+            let mods = elem.modify;
+            mods.forEach(function(mod)
+            {
+                let e = undefined
+                let styletoset = mod.style;
+                let locby = mod.locatetagby;
+                let nam = mod.makrname;
+                let act = mod.action;
+                if(locby == "class") {
+                    e = getSingleElementOfClass(nam);
+                }
+                if(locby == "id") {
+                    e = getSingleElementOfId(nam);
+                }
+                if(locby == "body") {
+                    e = document.body;
+                }
+                if(locby == "custom") {
+                    let xcondit = "//*[contains(text(), '"
+                    xcondit += nam
+                    xcondit += "')]"                    
+                    let blockelems = document.evaluate(xcondit, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
+                    if(blockelems.snapshotLength == 0)
+                        return;
+                    
+                    arre = []
+                    for( let co = 0; co < blockelems.snapshotLength; co++) {
+                        let node = blockelems.snapshotItem(co)
+                        if(node.nodeType != 1)
+                            continue;
+                        let cure = getIndependentParent(node, 8)
+                        if(!arre.includes(cure))
+                            arre.push(cure)
+                    }
+                    if(arre.length == 1)
+                        e = arre[0]
+                }
+                if(e != undefined)
+                {
+                    console.log("E = " + e);
+                    
+                    if(act == "remove") {
+                        let p = e.parentNode;
+                        p.removeChild(e);
+                    }
+                    if(act == "clearstyle") {
+                        e.removeAttribute("style")
+                    }
+                    if(act == "setstyle") {
+                        e.style = styletoset
+                    }		
+                }
+            })
+        }
+    })
 }
 
 let observer = new MutationObserver(mutationCallback);
@@ -63,19 +83,51 @@ observer.observe(document.body, config);
 
 function getSingleElementOfClass(clname)
 {
-  var elems = document.getElementsByClassName(clname);
-  if(elems.length == 1)
-    {
-      var itm = elems.item(0);
-      return itm;      
+    let elems = document.getElementsByClassName(clname);
+    if(elems.length == 1) {
+        let itm = elems.item(0)
+        return itm;
     }
 }
 
 function getSingleElementOfId(id)
 {
-  var elem = document.getElementById(id);
-  if(elem != null)
-    {
-      return elem;
+    let elem = document.getElementById(id)
+    if(elem != null) {
+        return elem;
     }
+}
+
+/*! 
+ * getNodeDepth is a recurcive function that calculates the distance betwen the current node and is's most distant leave. If node is leave itself, distance set to 1
+ */
+function getNodeDepth(node) {
+    if(!node.hasChildNodes)
+        return 1;
+    
+    let echildren = node.childNodes
+    let maxdepth = 0
+    for (const cnode of echildren) {
+        curd = getNodeDepth(cnode)
+        if(maxdepth < curd)
+            maxdepth = curd
+    }
+    return maxdepth+1;
+}
+
+function getIndependentParent(node, maxdist) {
+    if(node == document)
+        return node;
+    
+    let itsibs = [...node.parentElement.children].filter(child => child !== node);
+    let curdepth = 0
+    for (const e of itsibs) {
+        let d = getNodeDepth(e)
+        if( d > curdepth)
+            curdepth = d
+    }
+    if(curdepth > maxdist)
+        return node;
+    
+    return getIndependentParent(node.parentElement, maxdist);
 }
